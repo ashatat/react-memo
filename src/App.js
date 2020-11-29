ab43fb342e3fb1f8a85ccd8c09e2bec3f73bab43
Ahmed Shatat <a.shatat@hotmail.com>
2020-11-29 13:49:40 +0200

    just for testing (#2)
    * just for testing
    
    #1
    
    * testing add anther commit
    
    #1
    
    Co-authored-by: Rola gh <rola@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+import Input from './components/Input';
+
 function App() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const [giphyState] = useFetchData('https://gifphy/', []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {giphyState.loading && 'loading...'}
+      <Input
+        name="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      {giphyState.error && giphyState.error}
     </div>
   );
+}
+
+/**
+ * custom hook to fetch data on the first render
+ * @param {string} url url  to fetch data from api
+ * @param {any} initState the initial state
+ * @returns [state, error]
+ */
+function useFetchData(url, initState) {
+  const [data, setState] = useState(initState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    let mount = true;
+    async function fetData() {
+      try {
+        setLoading(true);
+        const { data } = await axios.get(url);
+        if (mount) {
+          setLoading(false);
+          setState(data);
+        }
+      } catch (error) {
+        if (mount) {
+          setLoading(false);
+          setError('aaaaaa');
+        }
+      }
+    }
+
+    fetData();
+    return () => {
+      mount = false;
+    };
+  }, [url]);
+
+  const state = {
+    data,
+    error,
+    loading,
+  };
+  return [state];
 }
 
 export default App;
